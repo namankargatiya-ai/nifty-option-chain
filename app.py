1b5020948baaa046b32bb20a02ea77f6c8d1d10e
namankargatiya-ai <namankargatiya@gmail.com>
2026-06-25 20:49:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import requests
 import pandas as pd
+from streamlit_autorefresh import st_autorefresh
+
+st_autorefresh(interval=30000, key="refresh")
 
 ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiI2OEFKUVUiLCJqdGkiOiI2YTNkM2NhNzNkZmE2NTYzZTA2NzI4M2YiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzgyMzk4MTE5LCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3ODI0MjQ4MDB9.0R1cqy9E4XTz3W-dm2nWMv82gmjA0JQYumtTd0vZvYs"
 
